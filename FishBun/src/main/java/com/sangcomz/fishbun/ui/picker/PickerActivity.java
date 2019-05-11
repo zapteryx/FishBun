e16dc2b1292f0705c69c418b8d17839a28df6a28
Max <mingxuanlim@gmail.com>
2019-05-11 14:48:28 +0800

    Update PickerActivity.java

--- FishBun/src/main/java/com/sangcomz/fishbun/ui/picker/PickerActivity.java
+++ FishBun/src/main/java/com/sangcomz/fishbun/ui/picker/PickerActivity.java
@@ -6,7 +6,6 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
