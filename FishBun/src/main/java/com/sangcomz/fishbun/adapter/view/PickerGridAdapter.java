2048a6b5d129f6ebe4e03bc00a98a8ac858d02b4
Max <mingxuanlim@gmail.com>
2019-05-11 14:47:38 +0800

    Update PickerGridAdapter.java

--- FishBun/src/main/java/com/sangcomz/fishbun/adapter/view/PickerGridAdapter.java
+++ FishBun/src/main/java/com/sangcomz/fishbun/adapter/view/PickerGridAdapter.java
@@ -13,7 +13,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
-import android.widget.Toast;
 
 import com.sangcomz.fishbun.Fishton;
 import com.sangcomz.fishbun.R;
